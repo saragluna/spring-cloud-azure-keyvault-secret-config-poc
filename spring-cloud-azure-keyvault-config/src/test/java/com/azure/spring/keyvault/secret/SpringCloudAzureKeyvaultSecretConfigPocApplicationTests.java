5d2d98e1c4fdcb78ed8b9324a405c4a0c038d808
Strong Liu(刘少壮) <shaozliu@microsoft.com>
2022-01-28 22:37:50 +0800

    Initial commit

---
+++ spring-cloud-azure-keyvault-config/src/test/java/com/azure/spring/keyvault/secret/SpringCloudAzureKeyvaultSecretConfigPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.spring.keyvault.secret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAzureKeyvaultSecretConfigPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
